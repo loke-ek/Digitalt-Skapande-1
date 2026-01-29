0e58dd0d4bdf248d814f17b1866c15b290a1c161
maya-wolf39 <wolfmaya0@gmail.com>
2026-01-29 10:32:10 +0100

    added an audiomanager

---
+++ DIG1 project/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] List<AudioSource> audioSources;
+    [SerializeField] List<AudioClip> audioClips;
+
+    public void PlaySound(int clipNumber)
+    {
+
+        foreach (AudioSource source in audioSources)
+        {
+            if (!source.isPlaying)
+            {
+                source.clip = audioClips[clipNumber];
+                source.Play();
+                return;
+            }
+        }
+
+    }
+
+}
+
