f8b1690a7a7f44a44f49ae645c6caf82c507d1b6
maya-wolf39 <wolfmaya0@gmail.com>
2026-01-29 12:17:12 +0100

    ignore

--- DIG1 project/Assets/Scripts/PickUpsK.cs
+++ DIG1 project/Assets/Scripts/PickUpsK.cs
@@ -2,24 +2,16 @@ using UnityEngine;
 
 public class PickUpsK : MonoBehaviour
 {
-    AudioManager audioManager;
-
-    public void Start()
-    {
-        FindAnyObjectByType<AudioManager>();
-    }
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            PlayKeyPickup();
+          
             Destroy(gameObject);
         }
     }
 
-    public void PlayKeyPickup()
-    {
-        audioManager.PlaySound(5);
-    }
+  
 }
