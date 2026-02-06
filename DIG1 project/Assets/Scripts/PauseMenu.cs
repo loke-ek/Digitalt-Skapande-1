1240479ef837662d30237d34a4227eda81b0291c
maya-wolf39 <wolfmaya0@gmail.com>
2026-02-06 15:05:32 +0100

    pause button

--- DIG1 project/Assets/Scripts/PauseMenu.cs
+++ DIG1 project/Assets/Scripts/PauseMenu.cs
@@ -1,50 +1,26 @@
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PauseMenu : MonoBehaviour
 {
-    InputAction pauseAction;
-
-    private bool isPaused;
 
     public GameObject pausePanel;
 
     public void Start()
     {
-        //so we can press "esc" and make the pause screen appear
-        pauseAction = InputSystem.actions.FindAction("Next");
-
-        isPaused = false;
     }
 
     public void Update()
     {
-        if (Input.GetButtonDown("Next"))
-        {
-            {
-                if (isPaused)
-                    ResumeGame();
-                else 
-                    PauseGame();
-            }
-        }
+        
     }
 
-
-    public void PauseGame()
+    public void Pause()
     {
-        Time.timeScale = 0;
-        pausePanel .SetActive(true);
-        isPaused = true;
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
     }
 
-    public void ResumeGame()
-    {
-        Time.timeScale = 1;
-        pausePanel.SetActive(false);
-        isPaused = false;
-    }
 
 }
 
