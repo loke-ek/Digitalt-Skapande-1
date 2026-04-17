6db43a0549bd45537eaa6e22ea8be1918645678f
EdithR3Y <edith.sjostrand@elev.ga.lbs.se>
2026-04-17 15:58:30 +0200

    buttons start in levels options

--- DIG1 project/Assets/Scripts/WinTrigger.cs
+++ DIG1 project/Assets/Scripts/WinTrigger.cs
@@ -6,7 +6,7 @@ public class WinTrigger : MonoBehaviour
 {
     public CanvasGroup fadeCanvas;
     public float fadeDuration = 2f;
-    public string winSceneName = "WinScene";
+    public string winSceneName = "LevelsOptions";
 
     private bool triggered = false;
 
@@ -42,6 +42,6 @@ public class WinTrigger : MonoBehaviour
 
         fadeCanvas.alpha = 1f;
 
-        SceneManager.LoadScene(winSceneName);
+        SceneManager.LoadScene("LevelsOptions");
     }
 }
