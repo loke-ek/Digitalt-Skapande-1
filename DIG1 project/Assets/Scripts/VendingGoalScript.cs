0fe7383822d59c9d6a3fdae05ba5b42af746d06f
loke-ek <lovisa.ek@elev.ga.lbs.se>
2026-04-27 13:59:57 +0200

    godis

--- DIG1 project/Assets/Scripts/VendingGoalScript.cs
+++ DIG1 project/Assets/Scripts/VendingGoalScript.cs
@@ -4,15 +4,23 @@ public class VendingGoalScript : MonoBehaviour
 {
     private float goalPosMin = -0.45f;
     private float goalPosMax = 0.45f;
+
+    private bool gameWon;
     
     void Start()
     {
-        
+        NewGoalPosition();
+
     }
 
 
     void Update()
     {
         
+    }
+
+    void NewGoalPosition()
+    {
+        float randomXValue = Random.Range(goalPosMin, goalPosMax);
     }
 }
